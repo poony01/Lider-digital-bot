6c822029e8b79c77b5e83047893fac10f4aea736
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-01 21:24:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from "express";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import dotenv from "dotenv";
-import bot from "./controllers/commandController.js";
-import "./controllers/messageController.js";
+import bot from "./controladores/commandController.js";
+import "./controladores/messageController.js";
 import TelegramBot from "node-telegram-bot-api";
 
 dotenv.config();
@@ -15,18 +15,18 @@ const __dirname = dirname(__filename);
 const TOKEN = process.env.BOT_TOKEN;
 const botInstance = new TelegramBot(TOKEN, { polling: false });
 
-// Rota principal só para teste
+// Rota de teste
 app.get("/", (req, res) => {
   res.send("🤖 Bot está rodando com sucesso!");
 });
 
-// ✅ Rota do Webhook
+// Webhook
 app.post(`/webhook/${TOKEN}`, express.json(), (req, res) => {
   botInstance.processUpdate(req.body);
   res.sendStatus(200);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORTA || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
