d433e7d97fa211a2cbca126f69f035ff4de88042
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-02 17:14:04 -0300

    Create videoService.js

---
+++ serviços/videoService.js
@@ -0,0 +1,8 @@
+// Serviço para geração de vídeos com IA.
+
+export default {
+  async criarVideo(prompt, usuarioId) {
+    // Integração futura com API de vídeo.
+    return "URL_DO_VIDEO_GERADO";
+  }
+};
