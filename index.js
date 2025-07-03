ab76bf43c42cdbe7a9cd5a88ecd86d0a1bd6e322
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-03 01:11:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 import TelegramBot from "node-telegram-bot-api";
+
+// Instância do bot Telegram. O token deve estar configurado nas variáveis de ambiente da Vercel.
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: false });
+
 export { bot };
