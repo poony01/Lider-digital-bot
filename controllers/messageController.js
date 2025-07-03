673df3585d926e2136c4497254641c56e063bed0
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-03 15:40:37 -0300

    Delete controllers directory

--- controllers/messageController.js
+++
@@ -1,8 +0,0 @@
-// Lida com mensagens comuns dos usuários (IA, voz, etc)
-export async function handleMessage(message, chatId, userId) {
-  // Aqui você vai integrar com o ChatGPT, checar limites, etc
-  // Exemplo de resposta simples:
-  // Envie para o Telegram usando sua função utilitária de envio
-  console.log(`Recebida mensagem de ${userId}: ${message.text}`);
-  // TODO: Implementar lógica de IA, limites, planos, etc
-}
