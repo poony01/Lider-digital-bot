a9293b3f66aabc23cb2f83b1c7970a034f6a5bbc
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-02 22:59:14 -0300

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -7,8 +7,7 @@ export default async function handler(req, res) {
     if (body.message && body.message.text === "/start") {
       const chatId = body.message.chat.id;
       const nome = body.message.chat.first_name || "usuário";
-
-      await bot.sendMessage(chatId, `✅ Olá ${nome}, o bot está funcionando via Webhook!`);
+      await bot.sendMessage(chatId, `Olá ${nome}, o bot está funcionando com Webhook! ✅`);
     }
 
     res.status(200).send("OK");
