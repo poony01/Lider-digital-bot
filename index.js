24c9429eac32958ef41ce34ac5d120f40b900cef
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-03 16:37:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import TelegramBot from "node-telegram-bot-api";
 
-// Token está nas variáveis da Vercel
+// Token vem das variáveis da Vercel
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: false });
 
 export { bot };
