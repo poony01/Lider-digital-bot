8a13576f19fef3f57128c3cc6d3c81342cc64a64
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-03 15:53:26 -0300

    Create webhook.js

---
+++ webhook.js
@@ -0,0 +1,17 @@
+import { bot } from "./index.js";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const msg = req.body.message;
+
+    if (msg?.text === "/start") {
+      const chatId = msg.chat.id;
+      const nome = msg.chat.first_name || "usuário";
+      await bot.sendMessage(chatId, `Olá ${nome}, o bot está funcionando com Webhook ✅`);
+    }
+
+    res.status(200).send("OK");
+  } else {
+    res.status(405).send("Method Not Allowed");
+  }
+}
