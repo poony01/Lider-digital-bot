2495eaf9393c35cbf808886bb12d6d8e1bf38f9e
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-03 16:35:05 -0300

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -2,14 +2,17 @@ import { bot } from "./index.js";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    const msg = req.body.message;
-    if (msg?.text === "/start") {
-      const chatId = msg.chat.id;
-      const nome = msg.chat.first_name || "usuário";
+    const mensagem = req.body.message;
+
+    if (mensagem && mensagem.text === "/start") {
+      const chatId = mensagem.chat.id;
+      const nome = mensagem.chat.first_name || "usuário";
+
       await bot.sendMessage(chatId, `Olá ${nome}, o bot está funcionando com Webhook ✅`);
     }
-    return res.status(200).send("ok");
-  }
 
-  return res.status(405).send("Method Not Allowed");
+    res.status(200).send("OK");
+  } else {
+    res.status(405).send("Method Not Allowed");
+  }
 }
