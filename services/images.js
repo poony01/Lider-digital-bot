71a1904c330ce9bc396433b0fc0192d7aad9b104
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-03 02:19:34 -0300

    Create images.js

---
+++ services/images.js
@@ -0,0 +1,6 @@
+// Serviço para geração e histórico de imagens (simulado)
+
+export async function gerarImagem(prompt) {
+  // Aqui você pode integrar com a API DALL-E da OpenAI ou outra API futuramente
+  return "URL_DA_IMAGEM_GERADA_EXEMPLO";
+}
