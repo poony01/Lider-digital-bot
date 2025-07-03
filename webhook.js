4912d682d2bfd3b7e44e0510c2bff8e85a2dec0f
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-03 01:18:24 -0300

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,5 +1,6 @@
 import { handleMessage } from "./controllers/messages.js";
 
+// Função handler para receber eventos do Telegram via webhook
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const mensagem = req.body.message;
