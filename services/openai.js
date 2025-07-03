3159a7dc2faea51d4c37b5fcc15ba21fdf724923
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-03 00:55:50 -0300

    Create openai.js

---
+++ services/openai.js
@@ -0,0 +1,6 @@
+export async function processarPergunta(pergunta) {
+  // Aqui você conecta com a API da OpenAI, exemplo:
+  // const resposta = await fetch(...);
+  // return resposta.data.choices[0].text;
+  return "Resposta da IA (exemplo)";
+}
