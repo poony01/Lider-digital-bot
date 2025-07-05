8f91d510fd4abc765eb017aca2e0b6478e083584
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-04 22:31:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ import TelegramBot from "node-telegram-bot-api";
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: false });
 
+// Define o webhook do Telegram apontando para sua URL da Vercel
+const URL = `https://lider-digital-bot.vercel.app`; // altere se seu domínio for outro
+bot.setWebHook(`${URL}/webhook/${process.env.BOT_TOKEN}`);
+
 export { bot };
