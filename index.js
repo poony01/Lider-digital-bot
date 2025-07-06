7117853459a034a6c2c72cf8662db5765b8f1128
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-05 23:55:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// index.js
 import TelegramBot from "node-telegram-bot-api";
+import { handleCallbackQuery } from "./controllers/callbackController.js";
 
 export const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: false });
+
+// 🔹 Trata cliques em botões inline
+bot.on("callback_query", async (query) => {
+  try {
+    await handleCallbackQuery(bot, query);
+  } catch (error) {
+    console.error("Erro ao tratar callback_query:", error);
+  }
+});
