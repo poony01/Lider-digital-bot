40e8aad6a099b02aa6409f2bfd22f1723cd8be54
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-06 01:59:45 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,4 @@
+// bot.js
 import TelegramBot from "node-telegram-bot-api";
 
 export const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: false });
-
-const WEBHOOK_URL = `https://${process.env.VERCEL_URL}/webhook`;
-
-bot.setWebHook(WEBHOOK_URL);
