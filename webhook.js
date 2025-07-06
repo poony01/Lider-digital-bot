1704ead6924ead0613acf5e9c58001cb0c127f21
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-06 00:24:54 -0300

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,12 +1,25 @@
-import { bot } from "./index.js";
-import { handleMessage } from "./controllers/messageController.js";
+// webhook.js
+import { bot } from './bot.js';
+import { handleMessage } from './controllers/messageController.js';
+import { handleCallback } from './controllers/callbackController.js';
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const msg = req.body.message;
-    if (msg) await handleMessage(bot, msg);
-    res.status(200).send("OK");
-  } else {
-    res.status(405).send("Method Not Allowed");
+  if (req.method === 'POST') {
+    const body = req.body;
+
+    // Trata mensagens comuns
+    if (body.message) {
+      await handleMessage(bot, body.message);
+    }
+
+    // Trata cliques em botões inline
+    if (body.callback_query) {
+      await handleCallback(bot, body.callback_query);
+    }
+
+    return res.status(200).send('OK');
   }
+
+  // Método inválido
+  return res.status(405).send('Method Not Allowed');
 }
