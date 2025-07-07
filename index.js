aae2f65f35a202ed773a477d9bdfe9dfd4163837
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-07 01:22:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// index.js
 import TelegramBot from 'node-telegram-bot-api';
 
+// Exporta o bot para ser usado no webhook
 export const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: false });
