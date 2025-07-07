72725a11a31dcd245fc679ae9481d1ceeff7e602
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-07 15:23:20 -0300

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,5 +1,6 @@
 import { bot } from './index.js';
 import { handleMessage } from './controllers/messageController.js';
+import { handleCallback } from './controllers/callbackController.js';
 
 export default async (req, res) => {
   if (req.method !== "POST") return res.status(200).send("🤖 Bot online");
@@ -9,6 +10,9 @@ export default async (req, res) => {
   try {
     if (update.message) {
       await handleMessage(bot, update.message);
+    }
+    if (update.callback_query) {
+      await handleCallback(bot, update.callback_query);
     }
   } catch (e) {
     console.error("Erro ao processar update:", e);
