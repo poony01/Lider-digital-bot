a3f1c71221143071b991db9f852125b1b277eda1
poony01 <ponnyycarvalho8486@gmail.com>
2025-07-12 01:34:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
 // index.js
 import TelegramBot from "node-telegram-bot-api";
-import { config } from "dotenv";
-import { tratarMensagem } from "./controllers/messageController.js";
-import { tratarCallbackQuery } from "./controllers/callbackController.js";
 
-config(); // Carrega variáveis .env
-
-export const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
-
-// Responde mensagens normais (ex: texto, comandos)
-bot.on("message", async (msg) => {
-  try {
-    await tratarMensagem(bot, msg);
-  } catch (e) {
-    console.error("❌ Erro ao tratar mensagem:", e.message);
-  }
-});
-
-// Responde cliques em botões inline
-bot.on("callback_query", async (callbackQuery) => {
-  try {
-    await tratarCallbackQuery(bot, callbackQuery);
-  } catch (e) {
-    console.error("❌ Erro no callback:", e.message);
-  }
+export const bot = new TelegramBot(process.env.BOT_TOKEN, {
+  polling: false
 });
